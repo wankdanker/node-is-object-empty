6d1525f11a96fa18afe9448684d4f6ac99b8ef90
Dan VerWeire <dverweire@gmail.com>
2017-06-12 14:04:01 -0400

    fix dates are not empty

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ module.exports = isObjectEmpty;
 function isObjectEmpty(obj) {
     var key;
 
+    if (obj instanceof Date) {
+        return false;
+    }
+
     for (key in obj) {
         if (obj.hasOwnProperty(key)) {
             return false;
--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 var isObjectEmpty = require('./')
     , assert = require('assert');
 
+var dt = new Date();
+
 assert.equal(isObjectEmpty({}), true);
 assert.equal(isObjectEmpty({ key : 'val' }), false);
+assert.equal(isObjectEmpty(dt), false);
